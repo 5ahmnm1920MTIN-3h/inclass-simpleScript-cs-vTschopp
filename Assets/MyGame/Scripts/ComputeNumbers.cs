419892bf88b88126c0c1ef261cd9be2d42eac5ef
NotMyTschopp <45820353+NotMyTschopp@users.noreply.github.com>
2019-12-05 14:18:08 +0100

    Finish project
    Substract numbers and enter it into text field. Print an error message if one input field is left empty. Add reset button.

---
+++ Assets/MyGame/Scripts/ComputeNumbers.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ComputeNumbers : MonoBehaviour
+{
+    public InputField input1;
+    public InputField input2;
+    public Text result;
+
+    float varA;
+    float varB;
+
+    public void SetResult()
+    {
+        result.text = CalculateNumbers();
+    }
+
+    string CalculateNumbers()
+    {
+        if (string.IsNullOrEmpty(input1.text) || string.IsNullOrEmpty(input2.text))
+        {
+            Debug.Log("ERROR 1: Number(s) missing.");
+
+            string outputError = "ERROR";
+            return outputError;
+        }
+        else
+        {
+            varA = float.Parse(input1.text);
+            varB = float.Parse(input2.text);
+
+            string outputSuccess = (varA - varB).ToString();
+            return outputSuccess;
+        }
+    }
+
+    public void ResetApp()
+    {
+        input1.text = "";
+        input2.text = "";
+        result.text = "";
+    }
+}
